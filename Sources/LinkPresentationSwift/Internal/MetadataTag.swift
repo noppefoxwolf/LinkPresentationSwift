58f9499d337518505ed46134271c3b20d2737f6a
noppe <noppelabs@gmail.com>
2026-05-26 20:38:28 +0900

    Use computed metadata tag values

--- Sources/LinkPresentationSwift/Internal/MetadataTag.swift
+++ Sources/LinkPresentationSwift/Internal/MetadataTag.swift
@@ -8,38 +8,46 @@ internal enum MetadataTag {
     case remoteVideoURL
     case icon
 
-    private static let propertyTypes: [String: MetadataTag] = [
-        "og:title": .title,
-        "og:image": .image,
-        "og:description": .description,
-        "og:video": .video,
-        "og:video:url": .remoteVideoURL,
-        "og:video:secure_url": .remoteVideoURL,
-        "og:icon": .icon,
-    ]
+    private static var propertyTypes: [String: MetadataTag] {
+        [
+            "og:title": .title,
+            "og:image": .image,
+            "og:description": .description,
+            "og:video": .video,
+            "og:video:url": .remoteVideoURL,
+            "og:video:secure_url": .remoteVideoURL,
+            "og:icon": .icon,
+        ]
+    }
 
-    private static let nameTypes: [String: MetadataTag] = [
-        "twitter:title": .title,
-        "twitter:image": .image,
-        "description": .description,
-        "twitter:description": .description,
-        "twitter:player": .video,
-        "twitter:player:stream": .remoteVideoURL,
-        "apple-touch-icon": .icon,
-    ]
+    private static var nameTypes: [String: MetadataTag] {
+        [
+            "twitter:title": .title,
+            "twitter:image": .image,
+            "description": .description,
+            "twitter:description": .description,
+            "twitter:player": .video,
+            "twitter:player:stream": .remoteVideoURL,
+            "apple-touch-icon": .icon,
+        ]
+    }
 
-    static let requiredEarlyStopTags: Set<String> = [
-        "og:title",
-        "og:image",
-    ]
+    static var requiredEarlyStopTags: Set<String> {
+        [
+            "og:title",
+            "og:image",
+        ]
+    }
 
-    static let videoEarlyStopTags: Set<String> = [
-        "og:video",
-        "og:video:url",
-        "og:video:secure_url",
-        "twitter:player",
-        "twitter:player:stream",
-    ]
+    static var videoEarlyStopTags: Set<String> {
+        [
+            "og:video",
+            "og:video:url",
+            "og:video:secure_url",
+            "twitter:player",
+            "twitter:player:stream",
+        ]
+    }
 
     static func from(property: String, name: String) -> MetadataTag? {
         if let type = propertyTypes[property] {
